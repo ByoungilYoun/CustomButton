101294deceee4f14830839a91466ed60787d4679
ByoungilYoun <jimmy7525@naver.com>
2020-07-21 03:34:28 +0900

    Make UIButton

--- CustomButton/CustomButton/ViewController.swift
+++ CustomButton/CustomButton/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+  //MARK: - Properties
+  private let myButton : UIButton = {
+    let bt = UIButton()
+    return bt
+  }()
 
+  //MARK: -  viewDidLoad()
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .red
+    
+    setUI()
+    setConstraint()
   }
 
-
+  //MARK: - setUI()
+  private func setUI() {
+    myButton.backgroundColor = .blue
+    view.addSubview(myButton)
+  }
+  
+  //MARK: - setConstraint()
+  private func setConstraint(){
+    myButton.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      myButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      myButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      myButton.widthAnchor.constraint(equalToConstant: 200),
+      myButton.heightAnchor.constraint(equalToConstant: 100)
+    ])
+  }
+  
 }
 
