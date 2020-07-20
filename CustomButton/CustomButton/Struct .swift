986592f5a81483ad7df5f3736abfd7a7dbabc5f6
ByoungilYoun <jimmy7525@naver.com>
2020-07-21 03:37:36 +0900

    Make ViewModel Struct

---
+++ CustomButton/CustomButton/Struct .swift
@@ -0,0 +1,14 @@
+//
+//  Struct .swift
+//  CustomButton
+//
+//  Created by 윤병일 on 2020/07/21.
+//  Copyright © 2020 Byoungil Youn. All rights reserved.
+//
+
+import Foundation
+
+struct MyCustomButtonViewModel {
+  let title : String
+  let image : String 
+}
