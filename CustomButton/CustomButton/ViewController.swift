bc8f1b49fb093e39ad749e3e7d30ee48e5e44088
ByoungilYoun <jimmy7525@naver.com>
2020-07-21 03:25:34 +0900

    Prepare project Delete SceneDelegate Delete MainStoryboard Set up AppDelegate

---
+++ CustomButton/CustomButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomButton
+//
+//  Created by 윤병일 on 2020/07/21.
+//  Copyright © 2020 Byoungil Youn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+
+
+}
+
